7cdbd83ef214699f204ecee669eae21898a6b17f
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-04-17 21:18:47 -0400

    add "mpi_dl_test"

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Class 20
+# Class 22
 
-This repository contains serves as basis to do the work in classes 20/21.
+This repository contains serves as basis to do the work in class 22.
 
 
 
---
+++ mpi/mpi_dl_test.c
@@ -0,0 +1,42 @@
+
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int
+main(int argc, char **argv)
+{
+  MPI_Init(&argc, &argv);
+  if (argc != 2) {
+    printf("Usage: %s <N>\nwhere <N> is the size of the message to exchange.\n",
+	   argv[0]);
+    exit(1);
+  }
+  int N = atoi(argv[1]);
+
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  assert(size == 2);
+
+  double *buf_send = calloc(N, sizeof(double));
+  double *buf_recv = calloc(N, sizeof(double));
+
+  if (rank == 0) {
+    MPI_Send(buf_send, N, MPI_DOUBLE, 1, 1234, MPI_COMM_WORLD);
+    MPI_Recv(buf_recv, N, MPI_DOUBLE, 1, 1234, MPI_COMM_WORLD,
+	     MPI_STATUS_IGNORE);
+  } else { // rank == 1
+    MPI_Send(buf_send, N, MPI_DOUBLE, 0, 1234, MPI_COMM_WORLD);
+    MPI_Recv(buf_recv, N, MPI_DOUBLE, 0, 1234, MPI_COMM_WORLD,
+	     MPI_STATUS_IGNORE);
+  }
+
+  free(buf_send);
+  free(buf_recv);
+
+  MPI_Finalize();
+  return 0;
+}
