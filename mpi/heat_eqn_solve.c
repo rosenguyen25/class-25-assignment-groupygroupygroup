a45ca1a15dfa91f8265d7ca59c52b001725104ab
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-04-20 09:25:54 -0400

    heat_eqn: parse options from the command line

--- mpi/heat_eqn_solve.c
+++ mpi/heat_eqn_solve.c
@@ -5,6 +5,9 @@
 #include <math.h>
 #include <assert.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <mpi.h>
 
 #define sqr(a) ((a) * (a))
@@ -12,19 +15,57 @@
 int
 main(int argc, char **argv)
 {
-  const int N = 100;
   const double L = 2. * M_PI;
-  const int n_timesteps = 200;
-  const int out_every = 10;
-  const double kappa = .01; 
+  int N = 100;
+  double max_time = 50.;
+  double out_every = 1.;
+  double kappa = .01;
 
   MPI_Init(&argc, &argv);
 
-  fld1d_option_write_single_file = true;
+  // parse options
+  opterr = 0;
+  int c;
+  while ((c = getopt (argc, argv, "sm:N:o:k:")) != -1) {
+    switch (c) {
+    case 's': fld1d_option_write_single_file = true; break;
+    case 'N': N = atoi(optarg); break;
+    case 'm': max_time = atof(optarg); break;
+    case 'o': out_every = atof(optarg); break;
+    case 'k': kappa = atof(optarg); break;
+    case '?':
+      if (optopt == 'N' || optopt == 'm' || optopt == 'o' || optopt == 'k') {
+        fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+      } else {
+        fprintf (stderr, "Unknown option '-%c'.\n", optopt);
+      }
+      return 1;
+    default:
+      abort();
+    }
+  }
 
   double dx = L / N;
   double dt = .5 * dx * dx / kappa; // pick dt to satisfy CFL condition
 
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  
+  if (rank == 0) {
+    printf("Parameters:\n");
+    printf("  -s                use single file output (%s)\n",
+	   fld1d_option_write_single_file ? "true" : "false");
+    printf("  -N <N>            number of grid points (%d)\n", N);
+    printf("  -m <max_time>     final time to run the simulation to (%g)\n", max_time);
+    printf("  -o <out_every>    output every so often; 0 for no output (%g)\n", out_every);
+    printf("  -k <kappa>        heat conductivity (%g)\n", kappa);
+    
+    printf("\n");
+    printf("Grid spacing dx  = %g\n", dx);
+    printf("Timestep     dt  = %g\n", dt);
+    printf("CFL number   CFL = %g\n", kappa * dt / sqr(dx));
+  }
+  
   struct fld1d *x = fld1d_create(N, 1);
   struct fld1d *rhs = fld1d_create(N, 0);
 
@@ -34,12 +75,22 @@ main(int argc, char **argv)
     F1(x, i) = exp(-sqr(xx - M_PI) / sqr(.5));
   }
 
-  for (int n = 0; n < n_timesteps; n++) {
-    // write out current solution every so many steps
-    if (n % out_every == 0) {
-      char fname[10];
-      sprintf(fname, "x%d", n);
+  // time integration loop
+  double tbeg = MPI_Wtime();
+  int n = 0; // step counter
+  int out_cnt = 0; // counter for output
+  double time_next_out = 0.; // time for next output
+  double time = 0.; // current time
+  while (time <= max_time) {
+    // write out current solution every so often
+    if (out_every && time >= time_next_out) {
+      char fname[100];
+      snprintf(fname, 100, "x%d", out_cnt++);
+      //printf("step %d time %g: Writing output \"%s\"\n", n, time, fname);
       fld1d_write(x, fname, dx);
+      while (time_next_out <= time) {
+        time_next_out += out_every;
+      }
     }
 
     // A simple forward Euler step x^{n+1} = x^{n} + dt * rhs(x^n)
@@ -50,8 +101,18 @@ main(int argc, char **argv)
 
     // then update solution: x += dt * rhs
     fld1d_axpy(x, dt, rhs);
+    
+    time += dt;
+    n++;
   }
 
+  double tend = MPI_Wtime();
+
+  if (rank == 0) {
+    printf("Integrated %d steps and wrote %d output files. Wall time = %g s\n",
+	   n, out_cnt, tend - tbeg);
+  }
+  
   fld1d_destroy(x);
   fld1d_destroy(rhs);
 
